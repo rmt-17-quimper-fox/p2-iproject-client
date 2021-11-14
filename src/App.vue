42d55bab1946844b11c01b8cd9d6b71a27c180fa
Andhika Bayu Mietra <andhikamietra@gmail.com>
2021-11-14 17:02:53 +0700

    update dev

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-
-    <div class="fb-login-button" data-width="" data-size="large" data-button-type="login_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
-
-<fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();" >
-</fb:login-button>
-
   </div>
 </template>
 

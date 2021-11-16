559df1b76f2c6b2bb3d27917047efc5ea38b4e08
ludfiwahyu <ludfiwahyuh@gmail.com>
2021-11-17 01:39:17 +0700

    Setup vue with vuex router

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Tweet></Tweet>
+  </div>
+</template>
+
+<script>
+import Tweet from '../components/Tweet.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Tweet,
+  }
+}
+</script>

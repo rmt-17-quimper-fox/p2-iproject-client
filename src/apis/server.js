9cfb2272e238e35a1ba8875082ce05d79dd23796
ludfiwahyu <ludfiwahyuh@gmail.com>
2021-11-18 10:34:31 +0700

    apis server baseURL

--- src/apis/server.js
+++ src/apis/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3000'
-    // baseURL: `https://news-portal-hacktiv8.herokuapp.com`
+    // baseURL: 'http://localhost:3000'
+    baseURL: `https://tweettweet8.herokuapp.com/`
 })

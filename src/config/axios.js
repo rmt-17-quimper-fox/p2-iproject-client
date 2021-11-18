8b25e482d9ba824ac44ab18448fc3a5ef332f15c
RaymondJTP <raymondpanjaitan11@gmail.com>
2021-11-18 11:14:36 +0700

    Done deploy

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://localhost:3000'
+    baseURL: 'https://findmehacktiv8.herokuapp.com'
+
 });
 
 export default instance;
